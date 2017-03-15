70c1ab6b24187d208d1d65dea61ed45d8b22d8da
Tri Vo <trong@google.com>
2017-03-15 09:19:10 -0700

    Mutation logic has been rewritten for readability and maintainability.
    Test: mmma test/vts-testcase/fuzz
    
    Change-Id: I99df15aea27e59182ba5060eeac9149bd8636dce

--- iface_fuzzer/ProtoFuzzerMutator.cpp
+++ iface_fuzzer/ProtoFuzzerMutator.cpp
@@ -15,67 +15,92 @@
  */
 
 #include "ProtoFuzzerMutator.h"
-#include "type_mutators/ProtoFuzzerArrayMutator.h"
-#include "type_mutators/ProtoFuzzerEnumMutator.h"
-#include "type_mutators/ProtoFuzzerHandleMutator.h"
-#include "type_mutators/ProtoFuzzerScalarMutator.h"
-#include "type_mutators/ProtoFuzzerStructMutator.h"
-#include "type_mutators/ProtoFuzzerUnionMutator.h"
-
 #include <iostream>
 
 using std::endl;
 using std::cerr;
 using std::cout;
 using std::make_unique;
+using std::unordered_map;
+using namespace std::placeholders;
 
 namespace android {
 namespace vts {
+namespace fuzzer {
 
 ProtoFuzzerMutator::ProtoFuzzerMutator(
-    Random &rand,
-    unordered_map<string, VariableSpecificationMessage> predefined_types,
-    const ProtoFuzzerMutatorBias &mutator_bias)
-    : mutator_bias_(mutator_bias),
-      rand_(rand),
-      predefined_types_(predefined_types) {
-  type_mutators_[TYPE_ARRAY] =
-      make_unique<ProtoFuzzerArrayMutator>(rand, this, predefined_types_);
-  type_mutators_[TYPE_ENUM] =
-      make_unique<ProtoFuzzerEnumMutator>(rand, this, predefined_types_);
-  type_mutators_[TYPE_HANDLE] =
-      make_unique<ProtoFuzzerHandleMutator>(rand, this, predefined_types_);
-  type_mutators_[TYPE_SCALAR] =
-      make_unique<ProtoFuzzerScalarMutator>(rand, this, predefined_types_);
-  type_mutators_[TYPE_STRUCT] =
-      make_unique<ProtoFuzzerStructMutator>(rand, this, predefined_types_);
-  type_mutators_[TYPE_UNION] =
-      make_unique<ProtoFuzzerUnionMutator>(rand, this, predefined_types_);
+    Random &rand, unordered_map<string, TypeSpec> predefined_types,
+    ProtoFuzzerMutatorBias mutator_bias)
+    : rand_(rand),
+      predefined_types_(predefined_types),
+      mutator_bias_(mutator_bias) {
+  // Default function used for mutation/random generation. Used for types for
+  // which the notion of mutation/random generation is not defined, e.g.
+  // TYPE_HANDLE, TYPE_HIDL_CALLBACK.
+  VarTransformFn default_transform =
+      [](const VariableSpecificationMessage &var_spec) {
+        return VariableSpecificationMessage{var_spec};
+      };
+
+  // Initialize random_gen_fns_ and mutate_fns_ tables.
+  random_gen_fns_[TYPE_ARRAY] =
+      std::bind(&ProtoFuzzerMutator::ArrayRandomGen, this, _1);
+  mutate_fns_[TYPE_ARRAY] =
+      std::bind(&ProtoFuzzerMutator::ArrayMutate, this, _1);
+
+  random_gen_fns_[TYPE_ENUM] =
+      std::bind(&ProtoFuzzerMutator::EnumRandomGen, this, _1);
+  mutate_fns_[TYPE_ENUM] = std::bind(&ProtoFuzzerMutator::EnumMutate, this, _1);
+
+  random_gen_fns_[TYPE_HANDLE] = default_transform;
+  mutate_fns_[TYPE_HANDLE] = default_transform;
+
+  random_gen_fns_[TYPE_HIDL_CALLBACK] = default_transform;
+  mutate_fns_[TYPE_HIDL_CALLBACK] = default_transform;
+
+  random_gen_fns_[TYPE_SCALAR] =
+      std::bind(&ProtoFuzzerMutator::ScalarRandomGen, this, _1);
+  mutate_fns_[TYPE_SCALAR] =
+      std::bind(&ProtoFuzzerMutator::ScalarMutate, this, _1);
+
+  random_gen_fns_[TYPE_STRUCT] =
+      std::bind(&ProtoFuzzerMutator::StructRandomGen, this, _1);
+  mutate_fns_[TYPE_STRUCT] =
+      std::bind(&ProtoFuzzerMutator::StructMutate, this, _1);
+
+  random_gen_fns_[TYPE_UNION] =
+      std::bind(&ProtoFuzzerMutator::UnionRandomGen, this, _1);
+  mutate_fns_[TYPE_UNION] =
+      std::bind(&ProtoFuzzerMutator::UnionMutate, this, _1);
+
+  random_gen_fns_[TYPE_VECTOR] =
+      std::bind(&ProtoFuzzerMutator::VectorRandomGen, this, _1);
+  mutate_fns_[TYPE_VECTOR] =
+      std::bind(&ProtoFuzzerMutator::VectorMutate, this, _1);
 }
 
-ExecutionSpecificationMessage ProtoFuzzerMutator::RandomGen(
-    const InterfaceSpecificationMessage &iface_spec, size_t num_calls) {
-  ExecutionSpecificationMessage result{};
+ExecSpec ProtoFuzzerMutator::RandomGen(const IfaceSpec &iface_spec,
+                                       size_t num_calls) {
+  ExecSpec result{};
 
   for (size_t i = 0; i < num_calls; ++i) {
     size_t num_apis = iface_spec.api_size();
     size_t rand_api_idx = rand_(num_apis);
-    FunctionSpecificationMessage rand_api =
-        RandomGen(iface_spec.api(rand_api_idx));
+    FuncSpec rand_api = RandomGen(iface_spec.api(rand_api_idx));
     result.add_api()->Swap(&rand_api);
   }
 
   return result;
 }
 
-void ProtoFuzzerMutator::Mutate(const InterfaceSpecificationMessage &iface_spec,
-                                ExecutionSpecificationMessage *exec_spec) {
+void ProtoFuzzerMutator::Mutate(const IfaceSpec &iface_spec,
+                                ExecSpec *exec_spec) {
   bool coin_flip = rand_(2);
 
   if (coin_flip) {
     // Mutate a random function in execution.
     size_t idx = rand_(exec_spec->api_size());
-    const FunctionSpecificationMessage &rand_api = exec_spec->api(idx);
+    const FuncSpec &rand_api = exec_spec->api(idx);
     (*exec_spec->mutable_api(idx)) = Mutate(rand_api);
   } else {
     // Generate a random function call in place of randomly chosen function in
@@ -87,56 +112,58 @@ void ProtoFuzzerMutator::Mutate(const InterfaceSpecificationMessage &iface_spec,
   }
 }
 
-FunctionSpecificationMessage ProtoFuzzerMutator::RandomGen(
-    const FunctionSpecificationMessage &func_spec) {
-  FunctionSpecificationMessage result{func_spec};
+FuncSpec ProtoFuzzerMutator::RandomGen(const FuncSpec &func_spec) {
+  FuncSpec result{func_spec};
   // We'll repopulate arg field.
   result.clear_arg();
-
   for (const auto &var_spec : func_spec.arg()) {
-    VariableSpecificationMessage rand_var_spec = RandomGen(var_spec);
+    VarInstance rand_var_spec = RandomGen(var_spec);
     auto *new_var = result.add_arg();
     new_var->Swap(&rand_var_spec);
   }
-
   return result;
 }
 
-FunctionSpecificationMessage ProtoFuzzerMutator::Mutate(
-    const FunctionSpecificationMessage &func_spec) {
-  FunctionSpecificationMessage result{func_spec};
-
+FuncSpec ProtoFuzzerMutator::Mutate(const FuncSpec &func_spec) {
+  FuncSpec result{func_spec};
   size_t num_args = result.arg_size();
   if (num_args > 0) {
     size_t rand_arg_idx = rand_(num_args);
-    VariableSpecificationMessage rand_arg = Mutate(result.arg(rand_arg_idx));
+    VarInstance rand_arg = Mutate(result.arg(rand_arg_idx));
     result.mutable_arg(rand_arg_idx)->Swap(&rand_arg);
   }
   return result;
 }
 
-VariableSpecificationMessage ProtoFuzzerMutator::RandomGen(
-    const VariableSpecificationMessage &var_spec) {
-  VariableSpecificationMessage result{};
-  result = FindTypeMutator(var_spec.type())->RandomGen(var_spec);
-  return result;
-}
-
-VariableSpecificationMessage ProtoFuzzerMutator::Mutate(
-    const VariableSpecificationMessage &var_spec) {
-  VariableSpecificationMessage result{};
-  result = FindTypeMutator(var_spec.type())->Mutate(var_spec);
-  return result;
-}
-
-ProtoFuzzerTypeMutator *ProtoFuzzerMutator::FindTypeMutator(VariableType type) {
-  auto type_mutator = type_mutators_.find(type);
-  if (type_mutator == type_mutators_.end()) {
-    cerr << "Type mutator not found for type: " << type << endl;
+static VariableSpecificationMessage Transform(
+    const VariableSpecificationMessage &var_spec,
+    unordered_map<VariableType, VarTransformFn> &transform_fns) {
+  auto type = var_spec.type();
+  auto transform_fn = transform_fns.find(type);
+  if (transform_fn == transform_fns.end()) {
+    cerr << "Transformation function not found for type: " << type << endl;
     exit(1);
   }
-  return type_mutator->second.get();
+  return transform_fn->second(var_spec);
 }
 
+VarInstance ProtoFuzzerMutator::RandomGen(const VarSpec &var_spec) {
+  return Transform(var_spec, random_gen_fns_);
+}
+
+VarInstance ProtoFuzzerMutator::Mutate(const VarInstance &var_instance) {
+  return Transform(var_instance, mutate_fns_);
+}
+
+const TypeSpec &ProtoFuzzerMutator::FindPredefinedType(string name) {
+  auto type_spec = predefined_types_.find(name);
+  if (type_spec == predefined_types_.end()) {
+    cerr << "Predefined type not found: " << name << endl;
+    exit(1);
+  }
+  return type_spec->second;
+}
+
+}  // namespace fuzzer
 }  // namespace vts
 }  // namespace android
