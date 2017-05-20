9f5fd85fc6f32cc22aecc15719b47be3e94025d9
Tri Vo <trong@google.com>
2017-05-20 12:32:26 -0700

    Move hal managing logic to separete class.
    This is done in preparation for nested interface support
    Test: SANITIZE_TARGET="address coverage" make vts -j64 && vts-tradefed
    run commandAndExit vts --skip-all-system-status-check --primary-abi-only
    --skip-preconditions -l VERBOSE --module VtsHalBluetoothV1_0IfaceFuzzer
    Change-Id: I3cf3eba18210562a975c7b9dc9255deeaf6157b8

--- iface_fuzzer/ProtoFuzzerMain.cpp
+++ iface_fuzzer/ProtoFuzzerMain.cpp
@@ -39,15 +39,12 @@ namespace fuzzer {
 
 // 64-bit random number generator.
 static Random random{static_cast<uint64_t>(time(0))};
-// Parameters that passed in to fuzzer.
+// Parameters that were passed in to fuzzer.
 static ProtoFuzzerParams params;
-// CompSpec corresponding to the interface targeted by
-// fuzzer.
-static CompSpec target_comp_spec;
-// VTS hal driver used to call functions of targeted interface.
-static unique_ptr<FuzzerBase> hal;
 // Used to mutate inputs to hal driver.
 static unique_ptr<ProtoFuzzerMutator> mutator;
+// Used to exercise HIDL HAL's API.
+static unique_ptr<ProtoFuzzerRunner> runner;
 
 static ProtoFuzzerMutatorConfig mutator_config{
     // Heuristic: values close to 0 are likely to be meaningful scalar input
@@ -76,24 +73,23 @@ static ProtoFuzzerMutatorConfig mutator_config{
 
 extern "C" int LLVMFuzzerInitialize(int *argc, char ***argv) {
   params = ExtractProtoFuzzerParams(*argc, *argv);
-  target_comp_spec =
-      FindTargetCompSpec(params.comp_specs_, params.target_iface_);
   mutator = make_unique<ProtoFuzzerMutator>(
       random, ExtractPredefinedTypes(params.comp_specs_), mutator_config);
-  hal.reset(InitHalDriver(target_comp_spec, params.binder_mode_));
+  runner = make_unique<ProtoFuzzerRunner>(params.comp_specs_);
+
+  runner->Init(params.target_iface_, params.binder_mode_);
   return 0;
 }
 
 extern "C" size_t LLVMFuzzerCustomMutator(uint8_t *data, size_t size,
                                           size_t max_size, unsigned int seed) {
-  ExecSpec exec_spec;
+  ExecSpec exec_spec{};
   if (!exec_spec.ParseFromArray(data, size)) {
     exec_spec =
-        mutator->RandomGen(target_comp_spec.interface(), params.exec_size_);
+        mutator->RandomGen(runner->GetOpenedIfaces(), params.exec_size_);
   } else {
-    mutator->Mutate(target_comp_spec.interface(), &exec_spec);
+    mutator->Mutate(runner->GetOpenedIfaces(), &exec_spec);
   }
-  cout << exec_spec.DebugString() << endl;
   exec_spec.SerializeToArray(data, exec_spec.ByteSize());
   return exec_spec.ByteSize();
 }
@@ -109,10 +105,11 @@ size_t LLVMFuzzerCustomCrossOver(const uint8_t *data1, size_t size1,
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   ExecSpec exec_spec;
-  if (!exec_spec.ParseFromArray(data, size) || exec_spec.api_size() == 0) {
+  if (!exec_spec.ParseFromArray(data, size) ||
+      exec_spec.function_call_size() == 0) {
     return 0;
   }
-  Execute(hal.get(), exec_spec);
+  runner->Execute(exec_spec);
   return 0;
 }
 
